455487354dfea729123bc2286aeed0bfe6012174
Nefertari Jones <nefertarijones5@gmail.com>
2022-06-28 15:36:19 -0400

    Initial Commit

---
+++ Ourglass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ourglass
+//
+//  Created by Nefertari Jones on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
