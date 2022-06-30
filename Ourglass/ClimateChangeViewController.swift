1caae17e9de5372b3a71febf73460d68675e5fed
Nefertari Jones <nefertarijones5@gmail.com>
2022-06-30 11:56:25 -0400

    update

---
+++ Ourglass/ClimateChangeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ClimateChangeViewController.swift
+//  Ourglass
+//
+//  Created by Nefertari Jones on 6/30/22.
+//
+
+import UIKit
+
+class ClimateChangeViewController: UIViewController {
+
+    @IBOutlet weak var listView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    var toDo = [String]()
+    @IBAction func buttonTapped(_ sender: Any) {
+        var item = listView.text
+        item = item + textField.text
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
